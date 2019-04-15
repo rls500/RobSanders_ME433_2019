18a0be2a39a3c8002719f14aa8a8986f0d4ee23b
rls500 <rlsandersla@gmail.com>
2019-04-15 14:15:49 -0500

    HW1 Project

---
+++ HW1/HW1.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: rlsan
+ *
+ * Created on April 15, 2019, 1:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
