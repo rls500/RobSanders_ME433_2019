a50aa06555d89251dc6904c36669c5abbaa2dfe2
rls500 <rlsandersla@gmail.com>
2019-04-15 16:10:24 -0500

    Proper Timing

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -17,25 +17,28 @@
 #pragma config OSCIOFNC = OFF // disable secondary osc
 #pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
 #pragma config FCKSM = CSDCMD // do not enable clock switch
-#pragma config WDTPS = 1:1 // use slowest wdt
+#pragma config WDTPS = PS1 // use slowest wdt
 #pragma config WINDIS = OFF // wdt no window mode
 #pragma config FWDTEN = OFF // wdt disabled
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
 #pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = MULL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
 #pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
 #pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
 #pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0x0rob // some 16bit userid, doesn't matter what
+#pragma config USERID = 0x0fab // some 16bit userid, doesn't matter what
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 #pragma config FUSBIDIO = ON // USB pins controlled by USB module
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
+
+//Constants
+#define CORE_TICKS 24000 // 24000 Ticks (1000 Hz)
 
 int main() {
 
@@ -55,14 +58,18 @@ int main() {
 
     // do your TRIS and LAT commands here
     //0 sets as input, 1 as output
-    TRISABITS.TRISA4=0;
-    TRISDBITS.TRISB4=1;
+    TRISAbits.TRISA4=0;
+    TRISBbits.TRISB4=1;
     //SETS OUTPUT TO HIGH
-    LATABITS.LATA4=1;
+    LATAbits.LATA4=1;
     __builtin_enable_interrupts();
 
     while(1) {
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
+        if(_CP0_GET_COUNT() >CORE_TICKS){
+            LATAINV = 0b00010000;
+            _CP0_SET_COUNT(0);     
+        }
     }
 }
